b0ce23c5f643c598c2cc0854525b8cce0d290a63
lamilekan263 <lamilekan263@gmail.com>
2020-09-24 22:05:09 +0100

    almost done with the porduct component

--- src/components/product.component/Product.js
+++ src/components/product.component/Product.js
@@ -1,21 +1,28 @@
 import React from 'react'
 
 import './Product.css'
-function Product() {
+function Product({ title, image, price, rating}) {
     return (
       <div className="product">
         <div className="product__info">
-          <p>The Lean stratup</p>
+                <p>{title}</p>
           <p className="product__price">
             <small>$</small>
-            <strong>19.99</strong>
+                    <strong>{price}</strong>
           </p>
-          <div className="product__rating">
-            <span role="img" aria-label="emo">⭐</span>
+                <div className="product__rating">
+                    {
+                        Array(rating)
+                            .fill()
+                            .map((_, i) =>(
+                            <span role="img" aria-label="emo">⭐</span>
+                        ) )
+                    }
+            
           </div>
         </div>
         <img
-          src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+          src={image}
           alt=""
             />
             <button>Add to Basket</button>
