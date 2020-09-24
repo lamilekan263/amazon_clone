a3ee216b277a5979fcf85bdfbc466074f0a11941
lamilekan263 <lamilekan263@gmail.com>
2020-09-24 19:44:40 +0100

    added home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import Header from './components/header.Component/Header'
-
+import Home from "./components/home.Component/Home";
 function App (){
     return(
         <div>
             <Header />
+            <Home />
         </div>
     )
 }
