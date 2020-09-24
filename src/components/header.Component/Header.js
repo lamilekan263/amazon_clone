fd2501147399854744b053f2e559baac37d7778c
lamilekan263 <lamilekan263@gmail.com>
2020-09-24 19:29:44 +0100

    added a material ui icons of both search and basket

--- src/components/header.Component/Header.js
+++ src/components/header.Component/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 import SearchIcon from "@material-ui/icons/Search";
-
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import "./Header.css";
 
 const Header = () => {
@@ -20,18 +20,20 @@ const Header = () => {
       <div className="header__nav">
         <div className="header__option">
           <span className="header__optionLineOne">Hello</span>
-          <span className="header__optionLineOne">Sign In</span>
+          <span className="header__optionLineTwo">Sign In</span>
         </div>
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
-          <span className="header__optionLineOne">& Orders</span>
+          <span className="header__optionLineTwo">& Orders</span>
         </div>
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
-          <span className="header__optionLineOne">Prime</span>
+          <span className="header__optionLineTwo">Prime</span>
         </div>
-        <div className="header__option">
-
+        <div className="header__optionBasket">
+          
+          <ShoppingBasketIcon />
+          <span className = "header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
