b3413fadb9bd96c709b91ae6e7954bf98bc1634f
lamilekan263 <lamilekan263@gmail.com>
2020-09-25 13:50:00 +0100

    completed the product page

--- src/components/home.Component/Home.js
+++ src/components/home.Component/Home.js
@@ -31,14 +31,36 @@ function Home() {
           />
         </div>
         <div className="home__row">
-                <Product
-                    
-                    image="https://images-na.ssl-images-amazon.com/images/I/810%2BGNdkzKL._AC_SY450_.jpg" />
-          <Product />
-          <Product />
+          <Product
+            id="48912536"
+            title="Samsung LC49RG90SSUXEN 49' Curvced LED Gaming Monitor"
+            price={199.99}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+          <Product
+            id="2389467"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            rating={5}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          />
+          <Product
+            id="907686546"
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation )"
+            price={598.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+          />
         </div>
         <div className="home__row">
-          <Product />
+          <Product
+            id="8765129093"
+            title="Samsung LC49RG90SSUXEN 49' Curvced LED Gaming Monitor - Super Ultra wide Dual WQHD 5120 x 1440"
+            price={1094.98}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+          />
         </div>
       </div>
     );
